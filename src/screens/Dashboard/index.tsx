7c4a8ee45c9b2f4577eeb641dfa3a52ff371d131
Thiago Minguim <Chazy249@gmail.com>
2021-12-14 14:08:49 -0300

    index dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HighlightCard } from '../../components/HighlightCard'
+import { TransactionCard } from '../../components/TransactionCard'
 
 import * as S from './styles'
 
@@ -43,6 +44,11 @@ export function Dashboard() {
           lastTransaction="01 à 16 de abril"
         />
       </S.HighlightCards>
+
+      <S.Transactions>
+        <S.Title> Listagem </S.Title>
+        <TransactionCard />
+      </S.Transactions>
     </S.Container>
   )
 }
