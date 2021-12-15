b9982be0607de20bd11437ae9d5622a6c4622d85
Thiago Minguim <Chazy249@gmail.com>
2021-12-15 09:41:57 -0300

    Styles Transaction and Dashboard

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components/native'
+import { FlatList } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+
+import { DataListProps } from '.'
 
 export const Container = styled.View`
   flex: 1;
@@ -66,7 +69,7 @@ export const Icon = styled(Feather)`
 export const HighlightCards = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
-  contentContainerStyle: { paddingHorizontal: 24 }
+  contentContainerStyle: { paddingHorizontal: 16 }
 })`
   width: 100%;
 
@@ -78,11 +81,18 @@ export const Transactions = styled.View`
   flex: 1;
   padding: 0 24px;
 
-  margin-top: ${RFPercentage(20)}px;
+  margin-top: ${RFPercentage(10)}px;
 `
 
 export const Title = styled.Text`
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.regular};
   margin-bottom: 16px;
+  margin-top: 19px;
 `
+export const TransactionList = styled(
+  FlatList as new () => FlatList<DataListProps>
+).attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: { paddingBottom: 5 }
+})``
