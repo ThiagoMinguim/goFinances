30623d424787276bb8f7af06480f7ed9442d66da
Thiago Minguim <Chazy249@gmail.com>
2021-12-15 10:23:32 -0300

    Input component

---
+++ src/components/Form/Input/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+import { TextInput } from 'react-native'
+
+export const Container = styled(TextInput)`
+  width: 100%;
+  padding: 18px 16px;
+
+  font-family: ${props => props.theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${props => props.theme.colors.text_dark};
+
+  background-color: ${props => props.theme.colors.shape};
+  border-radius: 5px;
+  margin-bottom: 8px;
+`
