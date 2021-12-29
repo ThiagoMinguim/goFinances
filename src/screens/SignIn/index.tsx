8ac274d49639abf07a3ef0db9632c99350c742a0
Thiago Minguim <thiago.minguim@gmail.com>
2021-12-29 10:58:44 -0300

    Wip: Tipando a importação do svg

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+import AppleSvg from '../../assets/apple.svg'
+import GoogleSvg from '../../assets/google.svg'
+import LogoSvg from '../../assets/logo.svg'
+
 import * as S from './styles'
 
 export function SignIn() {
-  return <S.Container></S.Container>
+  return (
+    <S.Container>
+      <S.Header>
+        <S.TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+          <S.Title>
+            Controle suas
+            {'\n'} finanças de forma {'\n'} muito mais simples
+          </S.Title>
+        </S.TitleWrapper>
+        <S.SignInTitle>
+          Faça seu login com {'\n'}uma das contas abaixo
+        </S.SignInTitle>
+      </S.Header>
+      <S.Footer></S.Footer>
+    </S.Container>
+  )
 }
