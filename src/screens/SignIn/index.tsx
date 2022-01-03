d57518019f2992dc0ef4a1006851fe32315f3c95
Thiago Minguim <thiago.minguim@gmail.com>
2022-01-03 11:01:58 -0300

    SignInSocial Button

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -5,6 +5,8 @@ import AppleSvg from '../../assets/apple.svg'
 import GoogleSvg from '../../assets/google.svg'
 import LogoSvg from '../../assets/logo.svg'
 
+import { SignInSocialButton } from '../../components/SignInSocialButton'
+
 import * as S from './styles'
 
 export function SignIn() {
@@ -13,16 +15,24 @@ export function SignIn() {
       <S.Header>
         <S.TitleWrapper>
           <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+
           <S.Title>
             Controle suas
             {'\n'} finanças de forma {'\n'} muito mais simples
           </S.Title>
         </S.TitleWrapper>
+
         <S.SignInTitle>
           Faça seu login com {'\n'}uma das contas abaixo
         </S.SignInTitle>
       </S.Header>
-      <S.Footer></S.Footer>
+
+      <S.Footer>
+        <S.FooterWrapper>
+          <SignInSocialButton title="Entrar com Google" svg={GoogleSvg} />
+          <SignInSocialButton title="Entrar com Apple" svg={AppleSvg} />
+        </S.FooterWrapper>
+      </S.Footer>
     </S.Container>
   )
 }
