fcf46bba4da6c726ed316f76d8c9656f72085b6b
Thiago Minguim <thiago.minguim@gmail.com>
2022-01-05 14:32:34 -0300

    index Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -44,12 +44,17 @@ export function Dashboard() {
     collection: DataListProps[],
     type: 'positive' | 'negative'
   ) {
+    const collectionFiltered = collection.filter(
+      transaction => transaction.type === type
+    )
+    if (collectionFiltered.length === 0) return 0
+
     const lastTransaction = new Date(
       Math.max.apply(
         Math,
-        collection
-          .filter(transaction => transaction.type === type)
-          .map(transaction => new Date(transaction.date).getTime())
+        collectionFiltered.map(transaction =>
+          new Date(transaction.date).getTime()
+        )
       )
     )
 
@@ -60,7 +65,7 @@ export function Dashboard() {
   }
 
   async function loadTransactions() {
-    const dataKey = '@gofinances:transactions'
+    const dataKey = `@gofinances:transactions_user:${user.id}`
     const response = await AsyncStorage.getItem(dataKey)
     const transactions = response ? JSON.parse(response) : []
 
@@ -108,7 +113,10 @@ export function Dashboard() {
       'negative'
     )
 
-    const totalInterval = `01 a ${lastTransactionExpensives}`
+    const totalInterval =
+      lastTransactionExpensives === 0
+        ? 'Não há transações'
+        : `01 a ${lastTransactionExpensives}`
 
     const total = entriesTotal - expensiveTotal
 
@@ -118,7 +126,10 @@ export function Dashboard() {
           style: 'currency',
           currency: 'BRL'
         }),
-        lastTransaction: `Última entrada dia ${lastTransactionEntries}`
+        lastTransaction:
+          lastTransactionEntries === 0
+            ? 'Não há transações'
+            : `Última entrada dia ${lastTransactionEntries}`
       },
 
       expensives: {
@@ -126,7 +137,10 @@ export function Dashboard() {
           style: 'currency',
           currency: 'BRL'
         }),
-        lastTransaction: `Última saída dia ${lastTransactionExpensives}`
+        lastTransaction:
+          lastTransactionExpensives === 0
+            ? 'Não há transações'
+            : `Última saída dia ${lastTransactionExpensives}`
       },
 
       total: {
