05e7695e2043924708989ca2e5927a14b43e339c
Thiago Minguim <thiago.minguim@gmail.com>
2022-01-05 09:11:38 -0300

    wip auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,7 @@
-import React, { createContext, ReactNode, useContext } from 'react'
+import React, { createContext, ReactNode, useContext, useState } from 'react'
+
+const { CLIENT_ID } = process.env
+const { REDIRECT_UTI } = process.env
 
 import * as AuthSession from 'expo-auth-session'
 
@@ -15,30 +18,45 @@ interface User {
 
 interface IAuthContextData {
   user: User
-  signInWithGoogle: () => void
+  signInWithGoogle(): Promise<void>
+}
+
+interface AuthorizationResponse {
+  params: {
+    access_token: string
+  }
+  type: string
 }
 
 const AuthContext = createContext({} as IAuthContextData)
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: '123',
-    name: 'John Doe',
-    email: 'okdaospkopsa'
-  }
+  const [user, setUser] = useState<User>({} as User)
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID =
-        '606207774632-rl11m065iph5evosrq2699q09ab8lg88.apps.googleusercontent.com'
-      const REDIRECT_UTI = 'https://auth.expo.io/@thiagominguim/gofinances'
       const RESPONSE_TYPE = 'token'
       const SCOPE = encodeURI('profile email')
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_UTI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
 
-      const response = await AuthSession.startAsync({ authUrl })
-      console.log(response)
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl
+      })) as AuthorizationResponse
+
+      if (type === 'success') {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        )
+        const userInfo = await response.json()
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture
+        })
+      }
     } catch (error) {
       throw new Error(error as any)
     }
