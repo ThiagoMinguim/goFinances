51edbd1233bb57dbc51292b8dd59656e09258222
Thiago Minguim <thiago.minguim@gmail.com>
2022-01-05 14:00:29 -0300

    Index signIn

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Alert } from 'react-native'
+import React, { useState } from 'react'
+import { ActivityIndicator, Alert } from 'react-native'
 import { RFValue } from 'react-native-responsive-fontsize'
+
+import { useTheme } from 'styled-components'
 
 import AppleSvg from '../../assets/apple.svg'
 import GoogleSvg from '../../assets/google.svg'
@@ -10,27 +12,37 @@ import { useAuth } from '../../hooks/auth'
 import { SignInSocialButton } from '../../components/SignInSocialButton'
 
 import * as S from './styles'
+import { is } from 'date-fns/locale'
 
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false)
+
   const { signInWithGoogle, signInWithApple } = useAuth()
+  const theme = useTheme()
 
   async function handleSignInWithGoogle() {
     try {
-      await signInWithGoogle()
+      setIsLoading(true)
+      return await signInWithGoogle()
     } catch (error) {
       console.log(error)
       Alert.alert('Não foi possível fazer o login Tente novamente')
+    } finally {
+      setIsLoading(false)
     }
   }
 
   async function handleSignInWithApple() {
     try {
-      await signInWithApple()
+      setIsLoading(true)
+      return await signInWithApple()
     } catch (error) {
       console.log(error)
       Alert.alert(
         'Não foi possível fazer o login Pela conta Apple, tente pelo Google'
       )
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -64,6 +76,13 @@ export function SignIn() {
             onPress={handleSignInWithApple}
           />
         </S.FooterWrapper>
+
+        {isLoading && (
+          <ActivityIndicator
+            color={theme.colors.shape}
+            style={{ marginTop: 18 }}
+          />
+        )}
       </S.Footer>
     </S.Container>
   )
